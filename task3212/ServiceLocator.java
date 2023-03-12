2fe6e95b122c96d011ff1a6a3ce369035141f904
Karina <karina-akhmadullina@mail.ru>
2023-03-13 00:24:07 +0300

    service locator

---
+++ task3212/ServiceLocator.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task32.task3212;
+
+import com.javarush.task.task32.task3212.contex.InitialContext;
+import com.javarush.task.task32.task3212.service.Service;
+
+public class ServiceLocator {
+    private static Cache cache;
+
+    static {
+        cache = new Cache();
+    }
+
+    public static Service getService(String jndiName) {
+        Service service = cache.getService(jndiName);
+        if (service != null)
+            return service;
+
+        InitialContext context = new InitialContext();
+        service = (Service) context.lookup(jndiName);
+        cache.addService(service);
+        return service;
+    }
+}
